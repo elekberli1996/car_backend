3ad17ae98c50a7c04e11495d0403548f7e37b7f0
ismayilovasgar <ismayilovv.asgarr.21@hotmail.com>
2025-01-11 12:52:41 +0400

    start projects

---
+++ static/js/base.js
@@ -0,0 +1,22 @@
+// ? Navbar Start --->
+const add = document.querySelector(".add");
+const elans = document.querySelector(".elans");
+const elan1 = document.querySelector(".elan1");
+const elan2 = document.querySelector(".elan2");
+
+add.addEventListener("click", () => {
+  console.log("s");
+  elans.classList.toggle("elansDisplay");
+  add.classList.toggle("addDisplay");
+});
+elan1.addEventListener("click", () => {
+  console.log("s");
+  elans.classList.toggle("elansDisplay");
+  add.classList.toggle("addDisplay");
+});
+elan2.addEventListener("click", () => {
+  console.log("s");
+  elans.classList.toggle("elansDisplay");
+  add.classList.toggle("addDisplay");
+});
+// ! Navbar End  --->
---
+++ static/js/home/home.js
@@ -0,0 +1,14 @@
+//? Home Start
+
+const filter = document.querySelector(".filterBtn");
+const filterDiv = document.querySelector(".filterDiv");
+
+filter.addEventListener("click", () => {
+  if (filterDiv.getAttribute("style")) {
+    filterDiv.removeAttribute("style");
+  } else {
+    filterDiv.setAttribute("style", "display:none");
+  }
+  filter.classList.toggle("black");
+});
+// ! Home End
